234cadeff81b74f9c6ba3da1707e015042c788b0
sam.silverberg@gmail.com <sam.silverberg@gmail.com>
2020-08-16 18:49:38 -0700

    updated notes

--- buildpb.sh
+++ buildpb.sh
@@ -3,3 +3,5 @@ git clone https://github.com/grpc/grpc-go.git
 cd grpc-go/cmd/protoc-gen-go-grpc
 go install .
 protoc -I ../sdfs/src/proto/ ../sdfs/src/proto/IOService.proto --go_out=sdfs --go-grpc_out=sdfs --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative
+git tag <tag>
+git push origin <tag>
