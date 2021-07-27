4411de1f620d027a81fb1e7578fe55b102c72fbc
sam.silverberg@gmail.com <sam.silverberg@gmail.com>
2021-07-26 19:51:37 -0700

    update random generator

--- utils/io.go
+++ utils/io.go
@@ -23,12 +23,13 @@ const (
 )
 
 func RandBytesMaskImpr(n int) []byte {
-	rand.Seed(time.Now().UTC().UnixNano())
+	source := rand.NewSource(time.Now().UnixNano())
+	generator := rand.New(source)
 	b := make([]byte, n)
 	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
-	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
+	for i, cache, remain := n-1, generator.Int63(), letterIdxMax; i >= 0; {
 		if remain == 0 {
-			cache, remain = rand.Int63(), letterIdxMax
+			cache, remain = generator.Int63(), letterIdxMax
 		}
 		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
 			b[i] = letterBytes[idx]
